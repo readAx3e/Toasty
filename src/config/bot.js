d4b1ec29de93b01ae54f3ea81bcf7a789246c97b
realAx3e <axeyvr66@gmail.com>
2026-05-26 14:16:42 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -6,7 +6,7 @@ export const botConfig = {
   // BOT PRESENCE (what users see under the bot name)
   // =========================
   // `status` options:
-  // - "not online"    = green dot
+  // - "dnd"    = green dot
   // - "idle"      = yellow moon
   // - "dnd"       = red do-not-disturb
   // - "invisible" = appears offline
