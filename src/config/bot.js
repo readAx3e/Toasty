9b0a8bf7dd17858d012e0fc54d71263e4de79930
realAx3e <axeyvr66@gmail.com>
2026-05-26 14:34:29 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "/helpㅤ|ㅤToasty,"
+        name: "/helpㅤ|ㅤToasty",
         // Activity type number (0 = Playing).
-        type: 3, 
+        type: (3), 
       },
     ],
   },
